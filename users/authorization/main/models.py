d88a67e75ede278280a042cf1ac84d73a5def10c
st2257st2257 <askristal@gmail.com>
2024-08-14 06:10:33 +0300

    port chech

--- users/authorization/main/models.py
+++ users/authorization/main/models.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from django.contrib.auth.models import AbstractUser
 
 
+class Access(models.Model):
+    description = models.CharField(max_length=255, blank=False)
+
+
 class Role(models.Model):
     name = models.CharField(max_length=255, blank=False)
     description = models.CharField(max_length=255, blank=False)
-
-
-class Access(models.Model):
-    description = models.CharField(max_length=255, blank=False)
+    access_list = models.ManyToManyField(Access)
 
 
 class InstituteGroup(models.Model):
@@ -23,4 +24,42 @@ class Preferences(models.Model):
 
 
 class User(AbstractUser):
-    name = models.CharField(max_length=255, blank=False)
+    third_name = models.CharField(max_length=150, blank=False)
+    token = models.CharField(max_length=255, blank=False)
+    book_rate = models.FloatField(default=7)
+    institute_group = models.ForeignKey(
+        InstituteGroup,
+        on_delete=models.CASCADE,
+        related_name="instituteGroup",
+        default=lambda: InstituteGroup.objects.get(id=1))
+    preferences = models.ManyToManyField(Preferences)
+    #user_role = models.ForeignKey(
+    #    Role,
+    #    on_delete=models.CASCADE,
+    #    related_name="user role")
+
+
+@admin.register(Role)
+class StatusAdmin(admin.ModelAdmin):
+    search_fields = ("id", "name", "description")
+    list_display = ("id", "name", )
+
+@admin.register(Access)
+class StatusAdmin(admin.ModelAdmin):
+    search_fields = ("id", "description")
+    list_display = ("id", )
+
+@admin.register(InstituteGroup)
+class StatusAdmin(admin.ModelAdmin):
+    search_fields = ("id", "name", "description")
+    list_display = ("id", "name", )
+
+@admin.register(Preferences)
+class StatusAdmin(admin.ModelAdmin):
+    search_fields = ("id", "name", "description")
+    list_display = ("id", "name", )
+
+@admin.register(User)
+class StatusAdmin(admin.ModelAdmin):
+    search_fields = ("id", "third_name")
+    list_display = ("id", "third_name", )
