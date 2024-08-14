2cee81354f456ed13506bd278b5231050c344b0e
st2257st2257 <askristal@gmail.com>
2024-08-14 19:16:07 +0300

    base info

--- users/authorization/main/views.py
+++ users/authorization/main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from .models import User, Access, Role, InstituteGroup, Preferences
+from .serializers import UserSerializer, AccessSerializer, RoleSerializer, InstituteGroupSerializer, PreferencesSerializer
 
+
+from rest_framework import permissions, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 
-class Index(APIView):
+class IndexAuth(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
@@ -22,3 +26,43 @@ class Index(APIView):
             'user_role': request.user.user_role.name
         }
         return Response(content)
+
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return self.filter_queryset(queryset)
+
+
+class InstituteGroupViewSet(viewsets.ModelViewSet):
+    queryset = InstituteGroup.objects.all()
+    serializer_class = InstituteGroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return self.filter_queryset(queryset)
+
+
+class AccessViewSet(viewsets.ModelViewSet):
+    queryset = Access.objects.all()
+    serializer_class = AccessSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return self.filter_queryset(queryset)
+
+
+class PreferencesViewSet(viewsets.ModelViewSet):
+    queryset = Preferences.objects.all()
+    serializer_class = PreferencesSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return self.filter_queryset(queryset)
