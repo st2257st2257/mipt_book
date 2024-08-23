7d14c8271d95306f6046ddec30f55081cb4938f4
st2257st2257 <askristal@gmail.com>
2024-08-23 19:53:21 +0300

    git

---
+++ deployment/keys/copy_keys.sh
@@ -0,0 +1,2 @@
+echo ../../backend/backend ../../email/emailservice ../../tg_app/tg_miptbook ../../users/authorization | xargs -n 1 cp mipt.key
+echo ../../backend/backend ../../email/emailservice ../../tg_app/tg_miptbook ../../users/authorization | xargs -n 1 cp mipt.crt
