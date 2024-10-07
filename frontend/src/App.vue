6c6d26fa062f0ce43e428f8af39522dc33cda297
st2257st2257 <askristal@gmail.com>
2024-10-07 23:16:59 +0300

    new

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,20 @@
 import TheFooter from "@/components/TheFooter.vue";
 import { provide } from 'vue';
 
-// provide('web_site', '127.0.0.1');
-provide('web_site', 'mipt.site');
+provide('web_site', '127.0.0.1');
+// provide('web_site', 'mipt.site');
 provide('web_site_register', 'mipt.site');
+
+
+function showNotification(notificationId: string) {
+    if (document.getElementById(notificationId)) {
+        document.getElementById(notificationId)?.classList.add("show");
+        document.getElementById(notificationId)?.classList.add("good_action");
+    } else {
+        console.error('Элемент с id "' + notificationId + '" не найден');
+    }
+}
+provide('notification', { showNotification });
 
 </script>
 
